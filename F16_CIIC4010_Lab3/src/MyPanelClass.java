267655fd2a9b096187996514789da4bd7f625063
4010d2 <4010d2@papaya.ece.uprm.edu>
2016-08-31 16:04:04 -0400

    Added method to draw an arrow & a star.

--- F16_CIIC4010_Lab3/src/MyPanelClass.java
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -24,20 +26,53 @@ public class MyPanelClass extends JPanel {
                         //Paint the background
                         g.setColor(Color.GRAY);
                         g.fillRect(x1, y1, width + 1, height + 1);
-//                        //Draw a border.
+//                       
+                          //Draw a border.
 //                        g.setColor(Color.BLACK);
 //                        g.drawRect(x1,  y1,  width, height);
-//                        //Draw a second border.
+//                      
+                          //Draw a second border.
 //                        g.setColor(Color.GREEN);
 //                        g.drawRect(x1+15, y1+15, width-30, height-30);
 //                        
+                          //Draw a line.
 //                        g.setColor(Color.WHITE);
 //                        g.drawLine(x1, y1, x2, y2);
 //                        
+                          //Draw a second line.
 //                        g.setColor(Color.BLUE);
 //                        g.drawLine(x2, y1, x1, y2);
                         
-                        g.setColor(Color.ORANGE);
-                        g.fillOval((x2-55)/2, (y2-55)/2, 55, 55);
+                          //Draw a circle.
+//                        g.setColor(Color.ORANGE);
+//                        g.fillOval((x2-55)/2, (y2-55)/2, 55, 55);
+                        
+                          //Draw an arrow pointing up.
+//                        Polygon p = new Polygon();
+//                        p.addPoint(x1 + 5, y1 + 25);
+//                        p.addPoint(x1 + 20, y1 + 10);
+//                        p.addPoint(x1 + 35, y1 + 25);
+//                        p.addPoint(x1 + 25, y1 + 25);
+//                        p.addPoint(x1 + 25, y1 + 45);
+//                        p.addPoint(x1 + 15, y1 + 45);
+//                        p.addPoint(x1 + 15, y1 + 25);
+//                        g.setColor(Color.WHITE);
+//                        g.fillPolygon(p);
+                        
+                          //Draw a star
+                          Polygon p2 = new Polygon();
+                          p2.addPoint(x1 + 25, y1 + 73);
+                          p2.addPoint(x1 + 41, y1 + 73);
+                          p2.addPoint(x1 + 47, y1 + 58);
+                          p2.addPoint(x1 + 53, y1 + 73);
+                          p2.addPoint(x1 + 69, y1 + 73);
+                          p2.addPoint(x1 + 56, y1 + 83);
+                          p2.addPoint(x1 + 61, y1 + 98);
+                          p2.addPoint(x1 + 47, y1 + 88);
+                          p2.addPoint(x1 + 34, y1 + 98);
+                          p2.addPoint(x1 + 38, y1 + 83);
+                          g.setColor(Color.WHITE);
+                          g.drawPolygon(p2);
+                        
             }
 }
